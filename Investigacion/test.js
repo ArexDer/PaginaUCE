6ecbe1a655c06f0b46775773dc67b5b7ae3ac6d9
JosuJosue6 <111206041+JosuJosue6@users.noreply.github.com>
2024-01-15 00:26:50 -0500

    test

---
+++ Investigacion/test.js
@@ -0,0 +1,10 @@
+document.getElementById("miBoton").addEventListener("click", function(){
+    var miPanel = document.getElementById("miPanel");
+    if (miPanel.style.display === "none") {
+        miPanel.style.display = "block";
+    } else {
+        miPanel.style.display = "none";
+    }
+ });
+
+ 
